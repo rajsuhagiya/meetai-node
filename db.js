c58c80aa1d7ab38144d216cb475c1eb8b53e2810
rajsuhagiya <https.helloworld@gmail.com>
2024-06-24 16:54:09 -0400

    add

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURL = "mongodb://localhost:27017/meetai";
+// const mongoURL = "mongodb://localhost:27017/meetai";
+const mongoURL = "mongodb+srv://meetai:meetai@cluster0.6ujy5fh.mongodb.net/";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURL);
