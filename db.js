350c30edc6e8fae8e5ec59d888aa72a436f18eda
rajsuhagiya <https.helloworld@gmail.com>
2024-07-01 13:34:56 -0400

    made changes on db.js

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 // const mongoURL = "mongodb://localhost:27017/meetai";
-const mongoURL = "mongodb+srv://meetai:meetai@cluster0.6ujy5fh.mongodb.net/";
+// const mongoURL = "mongodb+srv://meetai:meetai@cluster0.6ujy5fh.mongodb.net/";
+const mongoURL =
+  "mongodb+srv://meetai:meetai@cluster0.6ujy5fh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURL);
